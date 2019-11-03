3ae1280203732468c0d176d39b0a6c684d745b35
Rohit Sharma <sharmarohit@users.noreply.github.com>
2019-11-03 15:03:54 -0800

    Update index.py

--- variables/index.py
+++ variables/index.py
@@ -1,1 +1,21 @@
 
+# constants are uppercase
+# double underscore are called dunders. do not touch them
+
+some_value = 'its\' sunny'
+
+
+print(some_value)
+
+
+
+# formatted String
+name = 'RohitSharma'
+age = 41
+print(f'hi {name}, You are {age} years old')
+
+
+print(name[0::3])
+print(name.upper())
+
+print(bool(0))
